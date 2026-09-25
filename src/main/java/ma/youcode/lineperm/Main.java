42ef6a8b805fb1e5e18600f65264a860696b3bf8
KacimiLahcen <kacimi.lahcen88@gmail.com>
2026-09-25 11:41:52 +0000

    Implement DBConnection class and update Main to test database connection

--- src/main/java/ma/youcode/lineperm/Main.java
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -1,12 +1,22 @@
 package ma.youcode.lineperm;
 
 import ma.youcode.lineperm.ui.ConsoleApp;
+import ma.youcode.lineperm.db.DBConnection;
+import java.sql.Connection;
 
 	public class Main {
 
 		public static void main(String[] args) {
 
-			ConsoleApp appObj = new ConsoleApp();
-			appObj.start();
+			// ConsoleApp appObj = new ConsoleApp();
+			// appObj.start();
+
+			Connection connTest = DBConnection.getConnection();
+			if(connTest != null) {
+				System.out.println("Conn success");
+			} else {
+				System.out.println("Errour");
+			}
+
     		}
 }
---
+++ src/main/java/ma/youcode/lineperm/db/DBConnection.java
@@ -0,0 +1,25 @@
+package ma.youcode.lineperm.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:sqlite:data/audit.db";
+    private static Connection connection = null;
+
+    private DBConnection() {
+    }
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL);
+            }
+        } catch (SQLException e) {
+            System.out.println("pas connectes ");
+        }
+        return connection;
+    }
+
+}
